6937217f9cb5c068eab486643f2e21bfd0c660be
chronoDave <davidwolters6758@gmail.com>
2023-09-11 22:11:16 +0100

    Added renderer toolchain

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,7 @@ import { BrowserWindow, app } from 'electron';
 app.whenReady()
   .then(() => {
     const window = new BrowserWindow();
+    window.loadFile('renderer/index.html');
     return window.show();
   })
   .catch(console.error);
---
+++ src/renderer/index.tsx
@@ -0,0 +1,6 @@
+import * as forgo from 'forgo';
+
+forgo.mount(
+  <div>minibo</div>,
+  document.body
+);
